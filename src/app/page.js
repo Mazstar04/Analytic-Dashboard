9fdab3ba8cd7f04ae1e650f773d706d49ac0177a
Mazeedah Hamzat <mazeedahhamzat@gmail.com>
2024-01-25 19:31:08 +0100

    init

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="">
-    <button onClick={()=> window.location.href = "https://mazstar-dashboard.netlify.app/dashboard"}>Go to Dashboard</button>
+  Hy
    </div>
   );
 }
